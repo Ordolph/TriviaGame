3ab6d856e82451efd24d26dc3ca0535ea7f5b9f6
Ordolph <austin.t.randolph@gmail.com>
2018-10-15 17:11:12 -0400

    started prototyping on trivia questions

---
+++ assets/javascript/game.js
@@ -0,0 +1,105 @@
+let question1 = {
+    
+    question: "Pokemon, a series that changed many childhoods now features over 31 entries. ?",
+    answer1: "1995",
+    answer2: "1996",
+    answer3: "1998",
+    answer4:"2000",
+    correctAnswer: answer2,
+}
+
+let question2 = {
+    
+    question: "What is the best selling video game of all time?",
+    answer1: "Tetris",
+    answer2: "Wii Sports",
+    answer3: "Grand Theft Auto V",
+    answer4: "Minecraft",
+    correctAnswer: this.answer1,
+}
+
+let question3 = {
+    
+    question: "What is the best selling video game console of all time?",
+    answer1: "Nintendo Wii",
+    answer2: "Nintendo Game Boy",
+    answer3: "Xbox 360",
+    answer4: "Playstation 2",
+    correctAnswer: this.answer4,
+}
+
+let question4 = {
+    
+    question: "In 2008, a landmark occasion happened. Mario of the famous 'Mario Bros.' joined forces with his rival of nearly 20 years, Sonic the Hedgehog to do what?",
+    answer1: "Fight Bowser",
+    answer2: "Collect the Chaos Emeralds and defeat Eggman",
+    answer3: "Compete in the Summer Olympic Games",
+    answer4: "Fight in a tournament in Soul Caliber V",
+    correctAnswer: this.answer3,
+}
+
+let question5 = {
+    
+    question: "Nintendo's Super Smash brothers Ultimate features the largest cast of playable characters in the history of the franchise. Being hailed as the largest crossover in the history of video gaming. It contains many characters from non-Nintendo properties such as Metal Gear Solid's Snake, and Street Fighter's Ryu. Longtime fans however are most exited by a Nintendo character that had been requested for many years. Who is this character?",
+    answer1: "Tom Nook from Animal Crossing",
+    answer2: "Simon Belmont from Castlevania",
+    answer3: "King K. Rool from Donkey Kong",
+    answer4: "Ridley from Metroid",
+    correctAnswer: this.answer4,
+}
+
+let question6 = {
+    
+    question: "Many games are known for having silent protagonists. Who is the silent star of the Half-Life series?",
+    answer1: "Link",
+    answer2: "Gordon Freeman",
+    answer3: "Chel",
+    answer4: "Claude",
+    correctAnswer: this.answer2,
+}
+
+let question7 = {
+    
+    question: "1992's 'Wolfenstein 3D is regarded as being the first, marjorly popular first-person shooter. However another game released a year later, a shareware mod of Wolfenstein 3D was many times more popular, and still maintains popularity to this day. What is this game?",
+    answer1: "Wolfenstein Redux",
+    answer2: "DOOM",
+    answer3: "Quake",
+    answer4: "Duke Nukem 3D",
+    correctAnswer: this.answer2,
+}
+
+let question8 = {
+    
+    question: "Video gaming is full of acronyms, for instance; FPS(first-person shooter) and RTS(Real time Strategy). What does 4X stand for in video gaming?",
+    answer1: "Extend, Excavate, Execute, Exist",
+    answer2: "Exact, Excersise, Extrude, Excite",
+    answer3: "Explore, Expand, Eploit, Exterminate",
+    answer4: "Extrapolate, Expose, Explain, Exit",
+    correctAnswer: this.answer3,
+}
+
+let question9 = {
+    
+    question: "The Sony Playstation 3 sold better that expected. The reason was it was the cheapest version of something, what was it the cheapest version of?",
+    answer1: "A Blu-Ray player",
+    answer2: "A video game console",
+    answer3: "An internet router",
+    answer4: "A computer",
+    correctAnswer: this.answer1,
+}
+
+let question10 = {
+    
+    question: "When games are imported from Japan, they are often changed, sometimes is major ways. One such game, BioHazard had it's title changed. What was it's new title??",
+    answer1: "BioShock",
+    answer2: "System Shock",
+    answer3: "The Evil Within",
+    answer4: "Resident Evil",
+    correctAnswer: this.answer4,
+}
+
+let questions = [];
+
+questions.push(question1,  question2,  question3,  question4,  question5,  question6,  question7,  question8,  question9, question10 );
+
+console.log(questions);
